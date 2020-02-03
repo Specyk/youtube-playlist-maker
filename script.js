8eb8633dc91b02763ec511b4f2bbae1e474509b1
Emil Mordarski <krotock@gmail.com>
2020-02-03 21:56:01 +0100

    Updated script metadata and small fixes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YouTube Playlist Maker
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @namespace    YouTubePlaylistMaker
+// @version      0.2
+// @description  It adds extra button to create a youtube playlist from videos on YouTube page
+// @author       Emil Mordarski
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
@@ -16,9 +16,8 @@
     const baseUrl = "https://www.youtube.com/watch_videos?video_ids=";
     const makeIdsList = links => links.map(link => getVideoId(link)); // return youtube video ids[]
     const createPlaylistLink = ids => baseUrl + ids.join(','); // return youtube playlist link
-    const scan = DOM => scanStrategies[getLocation()].map(selector => DOM.querySelectorAll(selector)).flatMap(e => e); // return htmlElements: NodeList
+    const scan = DOM => scanStrategies[getLocation()].map(selector => Array.from(DOM.querySelectorAll(selector))).flat(1); // return array of HTML elements
     const getYtUrls = htmlElements => htmlElements.map(htmlEl => htmlEl.querySelector('a').href); // return youtube video urls[]
-    const placePlaylistLink = link => console.log(link);
     const addExtraBtn = (btn, DOM) => DOM.querySelector('ytd-app').querySelector('#buttons').appendChild(btn);
     const getLocation = () => location.pathname;
 
@@ -29,7 +28,7 @@
     }
 
     function goToPlaylist() {
-        let htmlElements = Array.from(scan(document)[0]);
+        let htmlElements = scan(document);
         console.log(htmlElements.length);
         let ytLinks = getYtUrls(htmlElements);
         let ids = makeIdsList(ytLinks);
