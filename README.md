d6e3ae4b06ebd4f7a81768ba56fa6dcbb864cf53
Emil Mordarski <mordarskiemil@gmail.com>
2022-04-24 00:39:54 +0200

    docs updated

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 This userscript adds a button to quickly create playlist link from videos on current YouTube page.
 
-It should works on most of youtube pages such as subscripptions, search results, trending etc.
+It should works on most of youtube pages such as subscriptions, search results, trending etc.
+
+It also can be extended for more pages. Object pageSelectionStrategy contains YouTube's pages paths and CSS selectors for look up videos.
 
 NOTE:
 Button might disappear when you navigate using links on YouTube. Refresh the page to restore the button.
+## Installation
+To use this script you need browser addon such as ViolentMonkey or TamperMonkey.
+
--- script.js
+++ script.js
@@ -10,74 +10,74 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+	"use strict";
 
-  const scanStrategies = new Map(
-    Object.entries(
-      // page path : selector
-      {
-        "/": "#video-title-link",
-        default: "#video-title",
-      }
-    )
-  );
+	const pageSelectionStrategy = {
+		// page path : selector
+		"/": "#video-title-link",
+		default: "#video-title",
+	}
 
-  const baseUrl = "https://www.youtube.com/watch_videos?video_ids=";
-  const makeIdsList = (links) => links.map((link) => getVideoId(link)); // return youtube video ids[]
-  const createPlaylistLink = (ids) => baseUrl + ids.join(","); // return youtube playlist link
+	const scanStrategies = new Map(
+		Object.entries(
+			pageSelectionStrategy
+		)
+	);
 
-  const scan = (DOM, locationSelector) =>
-    Array.from(DOM.querySelectorAll(scanStrategies.get(locationSelector))); // return array of HTML elements
-  const getYtUrls = (htmlElements) => htmlElements.map((htmlEl) => htmlEl.href); // return youtube video urls[]
+	const baseUrl = "https://www.youtube.com/watch_videos?video_ids=";
+	const makeIdsList = (links) => links.map((link) => getVideoId(link)); // return youtube video ids[]
+	const createPlaylistLink = (ids) => baseUrl + ids.join(","); // return youtube playlist link
 
-  const addExtraBtn = (btn, DOM) =>
-    DOM.querySelector("ytd-app").querySelector("#buttons").appendChild(btn);
+	const scan = (DOM, locationSelector) =>
+		Array.from(DOM.querySelectorAll(scanStrategies.get(locationSelector))); // return array of HTML elements
+	const getYtUrls = (htmlElements) => htmlElements.map((htmlEl) => htmlEl.href); // return youtube video urls[]
 
-  function getVideoId(link) {
-    let startIndex = link.indexOf("=") + 1;
-    return link.substring(startIndex).substr(0, 11); // id
-  }
+	const addExtraBtn = (btn, DOM) =>
+		DOM.querySelector("ytd-app").querySelector("#buttons").appendChild(btn);
 
-  function makePlaylist(dom, location) {
-    let htmlElements = scan(dom, location);
-    let ytLinks = getYtUrls(htmlElements);
-    let ids = makeIdsList(ytLinks);
-    let playlistLink = createPlaylistLink(ids);
-    window.location.href = playlistLink;
-  }
+	function getVideoId(link) {
+		let startIndex = link.indexOf("=") + 1;
+		return link.substring(startIndex).substr(0, 11); // id
+	}
 
-  function goToPlaylist() {
-    if (scanStrategies.get(location.pathname)) {
-      makePlaylist(document, location.pathname);
-    } else {
-      makePlaylist(document, "default");
-    }
-  }
+	function makePlaylist(dom, locationName) {
+		let htmlElements = scan(document, locationName);
+		console.log(htmlElements)
+		let ytLinks = getYtUrls(htmlElements);
+		let ids = makeIdsList(ytLinks);
+		let playlistLink = createPlaylistLink(ids);
+		window.location.href = playlistLink;
+	}
 
-  setTimeout(() => {
-    main();
-  }, 500);
+	function goToPlaylist() {
+		console.log(`location.pathname = ${location.pathname}`)
+		if (scanStrategies.get(location.pathname)) {
+			makePlaylist(document, location.pathname);
+		} else {
+			makePlaylist(document, "default");
+		}
+	}
 
-  const prepareBtn = () => {
-    const toPlaylistBtn = document.createElement("span");
-    toPlaylistBtn.id = "youtube-playlist-maker";
-    toPlaylistBtn.innerHTML = `<img style = 'width: 30px; height: 30px; cursor: pointer' src = 'https://pngimage.net/wp-content/uploads/2018/06/play-button-white-png-2.png'>`;
-    toPlaylistBtn.addEventListener("click", (e) => {
-      goToPlaylist();
-    });
-    return toPlaylistBtn;
-  };
+	main();
 
-  function main() {
-    const toPlaylistBtn = prepareBtn();
-    addExtraBtn(toPlaylistBtn, document);
-    setInterval(() => {
-      const ypmBtn = document.querySelector("#youtube-playlist-maker");
-      console.log(`ytmBtn = ${ypmBtn}`);
-      if (!ypmBtn) {
-        const newToPlaylistBtn = prepareBtn();
-        addExtraBtn(newToPlaylistBtn, document);
-      }
-    }, 1500);
-  }
+	const prepareBtn = () => {
+		const toPlaylistBtn = document.createElement("span");
+		toPlaylistBtn.id = "youtube-playlist-maker";
+		toPlaylistBtn.innerHTML = `<img style = 'width: 30px; height: 30px; cursor: pointer' src = 'https://pngimage.net/wp-content/uploads/2018/06/play-button-white-png-2.png'>`;
+		toPlaylistBtn.addEventListener("click", (e) => {
+			goToPlaylist();
+		});
+		return toPlaylistBtn;
+	};
+
+	function main() {
+		setInterval(() => {
+			const ypmBtn = document.getElementById("youtube-playlist-maker");
+			if (!ypmBtn) {
+				const newToPlaylistBtn = prepareBtn();
+				addExtraBtn(newToPlaylistBtn, document);
+			}
+		}, 1500);
+	}
 })();
+
